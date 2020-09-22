b8235434346530a38efe7d8492140f0ede069f7e
Michael Lee <michael.l33.1994@gmail.com>
2020-09-22 09:24:10 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -11,7 +11,7 @@ end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
-  test = {:name => "Grace Hopper}
+  test = {:name => "Grace Hopper"}
   p test
 end
 
